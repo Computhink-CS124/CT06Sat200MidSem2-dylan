a39161042ded5e9cfbd8f703c2984c8582b4c375
Computhink-CS124 <cs124@computhink.com.sg>
2025-08-30 06:44:05 +0000

    Aug 30, 2025, 6:44 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,12 @@
-
+function steup(){
+    createCanvas(600,400);
+    background(0);
+}
+function draw() {
+    ellipse(150,200,100,100);
+    triangle(300,250,350,150,250,150);
+    rect(550,200,100,100);
+    fill("black");
+    stroke("red");
+    strokeWeight(10);
+}
